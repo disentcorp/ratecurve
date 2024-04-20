f03b8a4a70aec158d5c7662d5d1be1db5d542679
chrisf <chris@disent.com>
2024-04-19 20:27:20 -0400

    improving equations tests

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -4,7 +4,11 @@ from dateroll import ddh, Duration
 from ratecurve.utils import *
 from ratecurve.equations import *
 Duration.just_bds = lambda self, *args,**kwargs: self.just_days
+import numpy as np
 
+# Shorthand for e, ln
+ln = np.log
+e = np.e
 class TestEquations(unittest.TestCase):
     @classmethod
     def setUpClass(cls): ...
@@ -20,9 +24,9 @@ class TestEquations(unittest.TestCase):
         d2 = ddh('t+1y')
         t = delta_t(d1, d2, dc, cal)
 
-        cap_factor(r,t, "EXP")
-        cap_factor(r, t, "LIN")
-        cap_factor(r, t, "YLD")
+        self.assertEqual(cap_factor(r,t, "EXP"),e**(r*t))
+        self.assertEqual(cap_factor(r, t, "LIN"),1+r*t)
+        self.assertEqual(cap_factor(r, t, "YLD"),(1+r)**t)
         with self.assertRaises(Exception):
             cap_factor(r, t, "apple")
 
@@ -34,7 +38,7 @@ class TestEquations(unittest.TestCase):
         d1 = ddh('t')
         d2 = ddh('t+1y')
         t = delta_t(d1, d2, dc, cal)       
-        disc_factor(r, t, "EXP")
+        self.assertEqual(disc_factor(r, t, "EXP"),1/(e**(r*t)))
 
 
     def test_convert_cap_factor_to_rate(self):
@@ -45,9 +49,9 @@ class TestEquations(unittest.TestCase):
         d2 = ddh('t+1y')
         t = delta_t(d1, d2, dc, cal)
 
-        convert_cap_factor_to_rate(cf, t, "EXP")
-        convert_cap_factor_to_rate(cf, t, "LIN")
-        convert_cap_factor_to_rate(cf, t, "YLD")
+        self.assertEqual(convert_cap_factor_to_rate(cf, t, "EXP"),ln(cf)/t)
+        self.assertEqual(convert_cap_factor_to_rate(cf, t, "LIN"),(cf-1)/t)
+        self.assertEqual(convert_cap_factor_to_rate(cf, t, "YLD"),(cf**(1/t)) - 1)
         with self.assertRaises(Exception):
             convert_cap_factor_to_rate(cf, t, "apple")
     
@@ -58,7 +62,7 @@ class TestEquations(unittest.TestCase):
         d1 = ddh('t')
         d2 = ddh('t+1y')
         t = delta_t(d1, d2, dc, cal)
-        convert_disc_factor_to_rate(df, t, "EXP")
+        self.assertEqual(convert_disc_factor_to_rate(df, t, "EXP"),(ln(1/df)/t))
 
 if __name__ == "__main__":
     unittest.main()
