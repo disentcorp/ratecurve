5af87345700f763ad4d6096f175c47ee5a042550
chrisf <chris@disent.com>
2024-04-22 11:04:53 -0400

    adding doc strings and restructuring imports in equations tests

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -1,13 +1,15 @@
 import unittest
 from dateroll import ddh, Duration
-from ratecurve.utils import *
-from ratecurve.equations import *
-Duration.just_bds = lambda self, *args,**kwargs: self.just_days
+from ratecurve import utils
+from ratecurve import equations
 import numpy as np
 
+
+Duration.just_bds = lambda self, *args,**kwargs: self.just_days
 # Shorthand for e, ln
 ln = np.log
 e = np.e
+
 class TestEquations(unittest.TestCase):
     @classmethod
     def setUpClass(cls): ...
@@ -16,52 +18,64 @@ class TestEquations(unittest.TestCase):
     def tearDownClass(self): ...
     
     def test_cap_factor(self):
+        '''
+        test cap factor equation across different methods
+        '''
         r = .05
         dc = "ACT/365"
         cal = "ALL"
         d1 = ddh('t')
         d2 = ddh('t+1y')
-        t = delta_t(d1, d2, dc, cal)
+        t = utils.delta_t(d1, d2, dc, cal)
 
-        self.assertEqual(cap_factor(r,t, "EXP"),e**(r*t))
-        self.assertEqual(cap_factor(r, t, "LIN"),1+r*t)
-        self.assertEqual(cap_factor(r, t, "YLD"),(1+r)**t)
+        self.assertEqual(equations.cap_factor(r,t, "EXP"),e**(r*t))
+        self.assertEqual(equations.cap_factor(r, t, "LIN"),1+r*t)
+        self.assertEqual(equations.cap_factor(r, t, "YLD"),(1+r)**t)
         with self.assertRaises(Exception):
-            cap_factor(r, t, "apple")
+            equations.cap_factor(r, t, "apple")
 
 
     def test_disc_factor(self):
+        '''
+        test disc factor equation across different methods
+        '''
         r = .05
         dc = "ACT/365"
         cal = "ALL"
         d1 = ddh('t')
         d2 = ddh('t+1y')
-        t = delta_t(d1, d2, dc, cal)       
-        self.assertEqual(disc_factor(r, t, "EXP"),1/(e**(r*t)))
+        t = utils.delta_t(d1, d2, dc, cal)       
+        self.assertEqual(equations.disc_factor(r, t, "EXP"),1/(e**(r*t)))
 
 
     def test_convert_cap_factor_to_rate(self):
+        '''
+        test conversion of cap factor to rate across different methods
+        '''
         cf = 1.05
         dc = "ACT/365"
         cal = "ALL"
         d1 = ddh('t')
         d2 = ddh('t+1y')
-        t = delta_t(d1, d2, dc, cal)
+        t = utils.delta_t(d1, d2, dc, cal)
 
-        self.assertEqual(convert_cap_factor_to_rate(cf, t, "EXP"),ln(cf)/t)
-        self.assertEqual(convert_cap_factor_to_rate(cf, t, "LIN"),(cf-1)/t)
-        self.assertEqual(convert_cap_factor_to_rate(cf, t, "YLD"),(cf**(1/t)) - 1)
+        self.assertEqual(equations.convert_cap_factor_to_rate(cf, t, "EXP"),ln(cf)/t)
+        self.assertEqual(equations.convert_cap_factor_to_rate(cf, t, "LIN"),(cf-1)/t)
+        self.assertEqual(equations.convert_cap_factor_to_rate(cf, t, "YLD"),(cf**(1/t)) - 1)
         with self.assertRaises(Exception):
-            convert_cap_factor_to_rate(cf, t, "apple")
+            equations.convert_cap_factor_to_rate(cf, t, "apple")
     
     def test_convert_disc_factor_to_rate(self):
+        '''
+        test conversion of disc factor to rate (methods already tested in cap factor)
+        '''
         df = .97
         dc = "ACT/365"
         cal = "ALL"
         d1 = ddh('t')
         d2 = ddh('t+1y')
-        t = delta_t(d1, d2, dc, cal)
-        self.assertEqual(convert_disc_factor_to_rate(df, t, "EXP"),(ln(1/df)/t))
+        t = utils.delta_t(d1, d2, dc, cal)
+        self.assertEqual(equations.convert_disc_factor_to_rate(df, t, "EXP"),(ln(1/df)/t))
 
 if __name__ == "__main__":
     unittest.main()
